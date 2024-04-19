d455f1c5dd0813a501cb75c455815f5e3455aa4f
Tushar Singhal <tsinghal451@gmail.com>
2024-04-19 18:07:35 +0000

    01_basic

---
+++ basic/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>First React App</h1>
+      <h2>Welcome To My World</h2>
+    </div>
+  );
+}
+
+export default App;
